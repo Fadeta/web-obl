4165d527a56e17b8c29a222697236311e9d986bb
Fadeta <fadeta287@gmail.com>
2024-08-29 18:29:07 +0700

    commit

--- backend-web-obl/index.js
+++ backend-web-obl/index.js
@@ -6,7 +6,7 @@ const dbPool = require ('./src/config/database');
 
 const usersRoutes = require('./src/routes/users')
 const middlewareLogRequest = require('./src/middleware/log');
-const upload = require('./src/middleware/multer');
+const uploadFile = require('./src/middleware/multer');
 
 const app = express();
 
@@ -15,12 +15,7 @@ app.use(express.json());
 app.use(cors({ origin: 'http://localhost:8080' }));
 
 app.use('/users', usersRoutes);
-app.post('/upload', upload.single('document'), (req, res) => {
-    res.json({
-        message: 'Upload berhasil'
-    });
-    
-})
+app.post('/upload-file', uploadFile);
 
 app.listen(PORT, () => {
     console.log(`Server berhasil di port ${PORT}`);
--- backend-web-obl/src/middleware/multer.js
+++ backend-web-obl/src/middleware/multer.js
@@ -1,5 +1,9 @@
 const multer = require('multer');
+const fs = require('fs');
+const dbPool = require('../config/database');
 const path = require('path');
+const buffer = require('buffer');
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -8,11 +12,67 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         const timestamp = new Date().getTime();
         const originalname = file.originalname;
-
         cb(null, `${timestamp}-${originalname}`);
     }
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage : storage });
 
-module.exports = upload;
+const uploadFile = async (req, res) => {
+    const file = req.file;
+        if (!file) {
+        res.status(400).send("File not found");
+        return;
+        }
+    const fileName = file.filename;
+    const fileType = file.mimetype;
+
+    try {
+        const { SPPH_BA_Visit, P0, P1, Quotation, Surat_Penunjukan, Kontrak_KB, KL_WO, Mitra_OBL, Dokumen_Pendukung } = req.body;
+    
+        const fileData = buffer.from(file.buffer).toString('base64');
+    
+        const db = await dbPool.getConnection();
+
+    const sql = `INSERT INTO document (
+        SPPH_BA_Visit,
+        P0,
+        P1,
+        Quotation,
+        Surat_Penunjukan,
+        Kontrak_KB,
+        KL_WO,
+        Mitra_OBL,
+        Dokumen_Pendukung
+    ) VALUES (
+        '${body.SPPH_BA_Visit}',
+        '${body.P0}',
+        '${body.P1}',
+        '${body.Quotation}',
+        '${body.Surat_Penunjukan}',
+        '${body.Kontrak_KB}',
+        '${body.KL_WO}',
+        '${body.Mitra_OBL}',
+        '${body.Dokumen_Pendukung}'
+    )`
+
+    const result = await db.query(sql, [SPPH_BA_Visit, P0, P1, Quotation, Surat_Penunjukan, Kontrak_KB, KL_WO, Mitra_OBL, Dokumen_Pendukung, fileData]); // Gunakan array untuk prepared statement
+
+    await db.release(); // Kembalikan koneksi ke pool
+
+    if (result.affectedRows > 0) {
+        res.status(200).send({
+        message: "File uploaded successfully",
+        file: fileName, // Ganti dengan path atau URL file sesuai storage
+        file_type: fileType,
+    });
+    } else {
+        res.status(500).send("Error saving data to database");
+    }
+} catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+}
+};
+
+module.exports = uploadFile;
